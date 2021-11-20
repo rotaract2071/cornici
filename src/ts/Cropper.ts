543feb405128d221be9ec27d8d602af0a1bac3b0
Daniele Ambrosino <mail@danieleambrosino.it>
2021-11-20 19:19:18 +0100

    chore: wrap actual ratio evaluation

--- src/ts/Cropper.ts
+++ src/ts/Cropper.ts
@@ -23,7 +23,18 @@ export default abstract class Cropper {
 
   static setAspectRatio(ratio: Ratio) {
     if (this.#cropper !== null)
-      this.#cropper.setAspectRatio(ratio === Ratio.Square ? 1 : ratio === Ratio.Landscape ? 3 / 2 : ratio === Ratio.Portrait ? 2 / 3 : null)
+      this.#cropper.setAspectRatio(this.#getActualRatio(ratio))
+  }
+
+  static #getActualRatio(ratio: Ratio): number {
+    switch (ratio) {
+      case Ratio.Square:
+        return 1;
+      case Ratio.Landscape:
+        return 3 / 2;
+        case Ratio.Landscape:
+          return 2 / 3;
+    }
   }
 
   static get croppedCanvas(): HTMLCanvasElement {

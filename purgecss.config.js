657633351b31c86eb16897d26cb19fb9a3d61367
Daniele Ambrosino <mail@danieleambrosino.it>
2021-11-21 17:30:17 +0100

    fix: cropper points bug

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     content: ['public/index.html'],
     css: ['public/style.css'],
-    safelist: [/^cropper/, /^line/],
+    safelist: [/^cropper/, /^line/, /^point/, /^dashed/],
     output: 'public'
 }

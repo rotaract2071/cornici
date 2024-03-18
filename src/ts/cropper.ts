4ee7d89cee80634e03c7b17396e22c179e059006
Daniele Ambrosino <mail@danieleambrosino.it>
2024-03-18 02:34:38 +0100

    chore: minor cropper refactoring

--- src/ts/cropper.ts
+++ src/ts/cropper.ts
@@ -1,28 +1,30 @@
-import CropperLib from "cropperjs";
+import Cropper from "cropperjs";
 import { Ratio } from "./constants.d";
 
-export async function initialize(file: File, image: HTMLImageElement, ratio: Ratio): Promise<CropperLib> {
+const ratios: Record<Ratio, number> = {
+	[Ratio.Square]: 1,
+	[Ratio.Landscape]: 3 / 2,
+	[Ratio.Portrait]: 2 / 3,
+};
+
+export async function initialize(file: File, image: HTMLImageElement, ratio: Ratio): Promise<Cropper> {
 	return new Promise((resolve) => {
 		const fileReader = new FileReader();
 		fileReader.onload = (e) => {
 			image.src = fileReader.result as string;
-			const cropper = new CropperLib(image, {
+			const cropper = new Cropper(image, {
 				zoomable: false,
 				viewMode: 2,
 				responsive: false,
 				background: false,
+				aspectRatio: ratios[ratio],
 			});
-			updateAspectRatio(cropper, ratio);
 			resolve(cropper);
 		};
 		fileReader.readAsDataURL(file);
 	});
 }
 
-export function updateAspectRatio(cropper: CropperLib, ratio: Ratio) {
-	cropper.setAspectRatio({
-		[Ratio.Square]: 1,
-		[Ratio.Landscape]: 3/2,
-		[Ratio.Portrait]: 2/3,
-	}[ratio]);
+export function updateAspectRatio(cropper: Cropper, ratio: Ratio) {
+	cropper.setAspectRatio(ratios[ratio]);
 }

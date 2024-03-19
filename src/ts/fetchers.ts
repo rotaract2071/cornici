b926b979802f93f01c4fece825bb26ef8c1ed451
Daniele Ambrosino <mail@danieleambrosino.it>
2024-03-19 18:07:27 +0100

    chore: improve CS

--- src/ts/fetchers.ts
+++ src/ts/fetchers.ts
@@ -1,52 +1,52 @@
 import { Logo, Ratio } from "./types.d";
 
 const logoHashes: Record<Logo, string> = {
-    [Logo.Distretto]: "06365",
-    [Logo.Doc]: "36231",
-    [Logo.Etruria]: "08921",
-    [Logo.Galileo]: "45306",
-    [Logo.Magnifico]: "13043",
-    [Logo.Montalbano]: "16037",
-    [Logo.Tirreno]: "63195",
+	[Logo.Distretto]: "06365",
+	[Logo.Doc]: "36231",
+	[Logo.Etruria]: "08921",
+	[Logo.Galileo]: "45306",
+	[Logo.Magnifico]: "13043",
+	[Logo.Montalbano]: "16037",
+	[Logo.Tirreno]: "63195",
 }
 
 const frameHashes: Record<Ratio, string> = {
-    [Ratio.Square]: "24840",
-    [Ratio.Landscape]: "28846",
-    [Ratio.Portrait]: "18461",
+	[Ratio.Square]: "24840",
+	[Ratio.Landscape]: "28846",
+	[Ratio.Portrait]: "18461",
 }
 
 export async function fetchLogo(logo: Logo): Promise<ImageBitmap | HTMLImageElement> {
-    const response = await fetch(`/logos/${logo}-${logoHashes[logo]}.png`);
-    return createImage(await response.blob());
+	const response = await fetch(`/logos/${logo}-${logoHashes[logo]}.png`);
+	return createImage(await response.blob());
 }
 
 export async function fetchFrame(ratio: Ratio): Promise<SVGElement> {
-    const response = await fetch(`/frames/${ratio}-${frameHashes[ratio]}.svg`);
-    return createSVG(await response.text());
+	const response = await fetch(`/frames/${ratio}-${frameHashes[ratio]}.svg`);
+	return createSVG(await response.text());
 }
 
 async function createImage(imageData: Blob): Promise<ImageBitmap | HTMLImageElement> {
-    if (window.hasOwnProperty("createImageBitmap")) {
-        return createImageBitmap(imageData);
-    }
-    const image = new Image();
-    image.src = await (async (blob: Blob): Promise<string> => {
-        return new Promise((resolve) => {
-            const reader = new FileReader();
-            reader.onloadend = () => resolve(reader.result as string);
-            reader.readAsDataURL(blob);
-        });
-    })(imageData);
-    await new Promise((resolve) => image.onload = resolve);
-    return image;
+	if (window.hasOwnProperty("createImageBitmap")) {
+		return createImageBitmap(imageData);
+	}
+	const image = new Image();
+	image.src = await (async (blob: Blob): Promise<string> => {
+		return new Promise((resolve) => {
+			const reader = new FileReader();
+			reader.onloadend = () => resolve(reader.result as string);
+			reader.readAsDataURL(blob);
+		});
+	})(imageData);
+	await new Promise((resolve) => image.onload = resolve);
+	return image;
 }
 
 function createSVG(svgText: string): SVGElement {
-    const frame: SVGElement = (() => {
-        const tmp = document.createElement("div");
-        tmp.innerHTML = svgText;
-        return tmp.firstElementChild as SVGElement;
-    })();
-    return frame;
+	const frame: SVGElement = (() => {
+		const tmp = document.createElement("div");
+		tmp.innerHTML = svgText;
+		return tmp.firstElementChild as SVGElement;
+	})();
+	return frame;
 }
--- src/ts/overlayer.ts
+++ src/ts/overlayer.ts
@@ -29,21 +29,44 @@ export default async function overlay(inputCanvas: HTMLCanvasElement, ratio: Rat
 	outputCanvas.height = outputCanvasHeight;
 
 	// Draw the cropped portion of the input image on the output canvas
-	drawImage(inputCanvas, outputCanvasContext, outputCanvasWidth, outputCanvasHeight);
+	drawImage(
+		inputCanvas,
+		outputCanvasContext,
+		outputCanvasWidth,
+		outputCanvasHeight,
+	);
 
 	const frame = await fetchFrame(ratio);
 	// Draw the frame on the output canvas
-	await drawFrame(frame, logo !== null ? colors[logo] : null, outputCanvasContext);
+	await drawFrame(
+		frame,
+		logo !== null ? colors[logo] : null,
+		outputCanvasContext,
+	);
 
 	const districtLogo = await fetchLogo(Logo.Distretto);
 	let drawnLogosCount = 0;
 	// Draw district's logo on the output canvas
-	drawLogo(districtLogo, logo !== null ? colors[logo] : colors[Logo.Distretto], drawnLogosCount++, outputCanvasContext, outputCanvasWidth, outputCanvasHeight);
+	drawLogo(
+		districtLogo,
+		logo !== null ? colors[logo] : colors[Logo.Distretto],
+		drawnLogosCount++,
+		outputCanvasContext,
+		outputCanvasWidth,
+		outputCanvasHeight,
+	);
 
 	if (logo !== null) {
 		const optionalLogo = await fetchLogo(logo);
 		// Draw the optional logo on the output canvas
-		drawLogo(optionalLogo, colors[logo], drawnLogosCount++, outputCanvasContext, outputCanvasWidth, outputCanvasHeight);
+		drawLogo(
+			optionalLogo,
+			colors[logo],
+			drawnLogosCount++,
+			outputCanvasContext,
+			outputCanvasWidth,
+			outputCanvasHeight,
+		);
 	}
 
 	// Create a URL to the rendered image encoded as PNG
@@ -56,7 +79,13 @@ export default async function overlay(inputCanvas: HTMLCanvasElement, ratio: Rat
 }
 
 function drawImage(inputCanvas: HTMLCanvasElement, outputCanvasContext: CanvasRenderingContext2D, outputCanvasWidth: number, outputCanvasHeight: number) {
-	outputCanvasContext.drawImage(inputCanvas, settings.frame.border, settings.frame.border, outputCanvasWidth - settings.frame.border * 2, outputCanvasHeight - settings.frame.border * 2);
+	outputCanvasContext.drawImage(
+		inputCanvas,
+		settings.frame.border,
+		settings.frame.border,
+		outputCanvasWidth - settings.frame.border * 2,
+		outputCanvasHeight - settings.frame.border * 2,
+	);
 }
 
 async function drawFrame(frame: SVGElement, customColor: string | null, outputCanvasContext: CanvasRenderingContext2D) {
@@ -86,10 +115,21 @@ function drawLogo(logo: ImageBitmap | HTMLImageElement, circleStrokeColor: strin
 	const [signX, signY] = getAxesSign(drawnLogosCount);
 
 	const [circleCenterX, circleCenterY] = getCircleCenterCoordinates(signX, signY, canvasWidth, canvasHeight);
-	drawLogoCircleBackground(circleCenterX, circleCenterY, circleStrokeColor, canvasContext);
+	drawLogoCircleBackground(
+		circleCenterX,
+		circleCenterY,
+		circleStrokeColor,
+		canvasContext,
+	);
 
 	const [dx, dy] = getLogoCoordinates(signX, signY, canvasWidth, canvasHeight);
-	canvasContext.drawImage(logo, dx, dy, settings.logo.image.side, settings.logo.image.side);
+	canvasContext.drawImage(
+		logo,
+		dx,
+		dy,
+		settings.logo.image.side,
+		settings.logo.image.side,
+	);
 }
 
 /**
@@ -100,16 +140,27 @@ function getAxesSign(drawnLogosCount: number): [number, number] {
 	// This is the angle relative to the center of the image that determines the position of the logo
 	// starting from the bottom right (-π/4) and stepping by 90° clockwise (-π/2)
 	const angle = -Math.PI / 4 - Math.PI / 2 * drawnLogosCount;
-	const [signX, signY] = [Math.cos, Math.sin].map((trigonometricFunction) => Math.sign(trigonometricFunction(angle)));
+	const [signX, signY] = [Math.cos, Math.sin]
+		.map((trigonometricFunction) => Math.sign(trigonometricFunction(angle)));
 	return [signX, signY];
 }
 
 function drawLogoCircleBackground(centerX: number, centerY: number, strokeColor: string, canvasContext: CanvasRenderingContext2D) {
 	canvasContext.beginPath();
-	canvasContext.ellipse(centerX, centerY, settings.logo.circle.radius, settings.logo.circle.radius, 0, 0, Math.PI * 2);
+	canvasContext.ellipse(
+		centerX,
+		centerY,
+		settings.logo.circle.radius,
+		settings.logo.circle.radius,
+		0,
+		0,
+		Math.PI * 2,
+	);
 	canvasContext.closePath();
+
 	canvasContext.fillStyle = settings.logo.circle.color;
 	canvasContext.fill();
+
 	canvasContext.strokeStyle = strokeColor;
 	canvasContext.lineWidth = settings.logo.circle.strokeWidth;
 	canvasContext.stroke();

a570fe354727f4a3a1161677635bb674bbe5ec8b
Daniele Ambrosino <mail@danieleambrosino.it>
2024-03-22 18:00:54 +0100

    chores

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -4,40 +4,18 @@ import downloadAndRevoke from "./downloader"
 import overlay from "./overlayer"
 import type { Logo, Ratio } from "./types"
 
-const form = document.querySelector("form")
-if (form === null) {
-	throw new Error("form element not found")
-}
-const fieldset = form.querySelector("fieldset")
-if (fieldset === null) {
-	throw new Error("fieldset element not found")
-}
-const imagesInput = fieldset.querySelector('input[name="images"]') as HTMLInputElement | null
-if (imagesInput === null) {
-	throw new Error("image input not found")
-}
-const ratioInput = fieldset.querySelector('select[name="ratio"]') as HTMLSelectElement | null
-if (ratioInput === null) {
-	throw new Error("ratio input not found")
-}
-const logoInput = fieldset.querySelector('select[name="logo"]') as HTMLSelectElement | null
-if (logoInput === null) {
-	throw new Error("logo input not found")
-}
-const applyButton = form.querySelector("button")
-if (applyButton === null) {
-	throw new Error("submit button not found")
-}
-const croppersDiv = document.querySelector(".croppers") as HTMLDivElement | null
-if (croppersDiv === null) {
-	throw new Error("croppers div not found")
-}
+const form = document.querySelector("form")!
+const fieldset = form.querySelector("fieldset")!
+const imagesInput = fieldset.querySelector('input[name="images"]') as HTMLInputElement
+const ratioInput = fieldset.querySelector('select[name="ratio"]') as HTMLSelectElement
+const logoInput = fieldset.querySelector('select[name="logo"]') as HTMLSelectElement
+const applyButton = form.querySelector("button")!
+const croppersContainer = document.getElementById("croppers") as HTMLDivElement
 
 const croppers = new Array<{ file: File, cropper: Cropper }>()
 
 const resetCroppers = () => {
-	croppersDiv.innerHTML = ""
-	croppersDiv.style.removeProperty("opacity")
+	croppersContainer.innerHTML = ""
 	croppers.length = 0
 }
 
@@ -54,7 +32,7 @@ imagesInput.addEventListener("change", async () => {
 		const wrapper = document.createElement("div")
 		const image = new Image()
 		wrapper.appendChild(image)
-		croppersDiv.appendChild(wrapper)
+		croppersContainer.appendChild(wrapper)
 		try {
 			const cropper = await initializeCropper(file, image, ratio)
 			croppers.push({ file, cropper })

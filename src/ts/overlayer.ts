ea31614628ab500270129e701fbb3186abf03846
Daniele Ambrosino <mail@danieleambrosino.it>
2024-06-29 16:44:32 +0200

    refactor: Overlayer class

--- src/ts/overlayer.ts
+++ src/ts/overlayer.ts
@@ -12,41 +12,144 @@ export default async function (
 	color: string | null,
 	logos: (ImageBitmap | HTMLImageElement)[],
 ): Promise<URL> {
-	const canvas = createCanvas(width, height)
-	const context = canvas.getContext("2d")
-	if (context === null) {
-		throw new Error("Canvas 2D rendering context is not supported")
+	const overlayer = new Overlayer(width, height)
+	overlayer.drawImage(image)
+	overlayer.drawFrame(frame, color)
+	for (const logo of logos) {
+		overlayer.drawLogo(logo, color ?? settings.colors[Logo.Distretto])
+	}
+	return new URL(URL.createObjectURL(await overlayer.getBlobAndDestroy()))
+}
+
+class Overlayer {
+	#canvas: OffscreenCanvas | HTMLCanvasElement
+	#context: OffscreenCanvasRenderingContext2D | CanvasRenderingContext2D
+	#drawnLogosCount = 0
+
+	constructor(width: number, height: number) {
+		this.#canvas = createCanvas(width, height)
+		const context = this.#canvas.getContext("2d")
+		if (context === null) {
+			throw new Error("Canvas 2D rendering context is not supported")
+		}
+		this.#context = context
 	}
 
-	// Draw the cropped portion of the input image on the output canvas
-	drawImage(
-		image,
-		context,
-		width,
-		height,
-	)
+	drawImage(image: HTMLCanvasElement) {
+		const x = settings.frame.border
+		const y = settings.frame.border
+		const width = this.#canvas.width - settings.frame.border * 2
+		const height = this.#canvas.height - settings.frame.border * 2
+		this.#context.drawImage(image, x, y, width, height)
+	}
 
-	// Draw the frame on the output canvas
-	drawFrame(
-		frame,
-		color,
-		context,
-	)
+	drawFrame(frame: Frame, color: string | null) {
+		for (const path of frame.paths) {
+			this.#context.fillStyle = color !== null && path.customizable ? color : path.fill
+			this.#context.fill(new Path2D(path.definition))
+		}
+	}
 
-	// Draw the logos
-	for (const [index, logo] of logos.entries()) {
-		drawLogo(
+	drawLogo(logo: ImageBitmap | HTMLImageElement, circleStrokeColor: string) {
+		const [signX, signY] = this.#getAxesSign()
+
+		this.#drawLogoCircleBackground(signX, signY, circleStrokeColor)
+
+		const [x, y] = this.#getLogoTopLeftCoordinates(signX, signY)
+		this.#context.drawImage(
 			logo,
-			color ?? settings.colors[Logo.Distretto],
-			index,
-			context,
-			width,
-			height,
+			x,
+			y,
+			settings.logo.image.side,
+			settings.logo.image.side,
 		)
+
+		++this.#drawnLogosCount
 	}
 
-	// Create a URL to the rendered image encoded as PNG
-	return new URL(URL.createObjectURL(await getBlobAndDestroy(canvas)))
+	#getAxesSign(): [number, number] {
+		// The angle relative to the center of the image that determines the position of the logo
+		// starting from the bottom right (-π/4) and stepping by 90° clockwise (-π/2)
+		const angle = -Math.PI / 4 - Math.PI / 2 * this.#drawnLogosCount
+		return [Math.cos, Math.sin].map((fn) => Math.sign(fn(angle))) as [number, number]
+	}
+
+	#drawLogoCircleBackground(signX: number, signY: number, strokeColor: string) {
+		const [centerX, centerY] = [
+			this.#canvas.width / 2 + signX * (this.#canvas.width / 2 - settings.logo.circle.margin - Math.floor(settings.logo.circle.strokeWidth / 2) - circleRadius),
+			this.#canvas.height / 2 - signY * (this.#canvas.height / 2 - settings.logo.circle.margin - Math.floor(settings.logo.circle.strokeWidth / 2) - circleRadius),
+		]
+
+		const compensationAngle = Math.PI / 64
+		const startAngle = Math.PI * 3 / 4
+
+		// Draw the portion of ellipse that serves as border
+		this.#context.beginPath()
+		this.#context.ellipse(
+			centerX,
+			centerY,
+			circleRadius + settings.logo.circle.strokeWidth,
+			circleRadius + settings.logo.circle.strokeWidth,
+			this.#drawnLogosCount * Math.PI / 2,
+			startAngle + compensationAngle,
+			startAngle + Math.PI - compensationAngle,
+		)
+		this.#context.closePath()
+		this.#context.fillStyle = strokeColor
+		this.#context.fill()
+
+		// Dirty hack to hide the remaining portion of the frame in the corner
+		const patchAngle = Math.PI / 16
+		this.#context.beginPath()
+		this.#context.moveTo(centerX, centerY)
+		this.#context.arc(
+			centerX,
+			centerY,
+			circleRadius + settings.logo.circle.strokeWidth + 4,
+			this.#drawnLogosCount * Math.PI / 2 + Math.PI / 4 - patchAngle / 2,
+			this.#drawnLogosCount * Math.PI / 2 + Math.PI / 4 + patchAngle / 2,
+		)
+		this.#context.closePath()
+		this.#context.fillStyle = settings.logo.circle.color
+		this.#context.fill()
+
+		// Draw the actual circle
+		this.#context.beginPath()
+		this.#context.arc(
+			centerX,
+			centerY,
+			circleRadius,
+			0,
+			Math.PI * 2,
+		)
+		this.#context.closePath()
+		this.#context.fillStyle = settings.logo.circle.color
+		this.#context.fill()
+	}
+
+	#getLogoTopLeftCoordinates(signX: number, signY: number): [number, number] {
+		return [
+			this.#canvas.width / 2 + signX * (this.#canvas.width / 2 - logoMargin - (signX > 0 ? settings.logo.image.side : 0)),
+			this.#canvas.height / 2 - signY * (this.#canvas.height / 2 - logoMargin - (signY < 0 ? settings.logo.image.side : 0)),
+		]
+	}
+
+	async getBlobAndDestroy(): Promise<Blob> {
+		const canvas = this.#canvas
+		if (canvas instanceof HTMLCanvasElement) {
+			return new Promise((resolve, reject) => {
+				canvas.toBlob((blob) => {
+					if (blob === null) {
+						reject()
+						return
+					}
+					canvas.remove()
+					resolve(blob)
+				})
+			})
+		}
+		return canvas.convertToBlob()
+	}
 }
 
 function createCanvas(width: number, height: number): OffscreenCanvas | HTMLCanvasElement {
@@ -58,175 +161,6 @@ function createCanvas(width: number, height: number): OffscreenCanvas | HTMLCanv
 		canvas.height = height
 		return canvas
 	}
-}
-
-function drawImage(
-	image: HTMLCanvasElement,
-	context: OffscreenCanvasRenderingContext2D | CanvasRenderingContext2D,
-	width: number,
-	height: number,
-) {
-	context.drawImage(
-		image,
-		settings.frame.border,
-		settings.frame.border,
-		width - settings.frame.border * 2,
-		height - settings.frame.border * 2,
-	)
-}
-
-function drawFrame(
-	frame: Frame,
-	color: string | null,
-	context: OffscreenCanvasRenderingContext2D | CanvasRenderingContext2D,
-) {
-	for (const path of frame.paths) {
-		context.fillStyle = color !== null && path.customizable ? color : path.fill
-		context.fill(new Path2D(path.definition))
-	}
-}
-
-function drawLogo(
-	logo: ImageBitmap | HTMLImageElement,
-	circleStrokeColor: string,
-	drawnLogosCount: number,
-	context: OffscreenCanvasRenderingContext2D | CanvasRenderingContext2D,
-	width: number,
-	height: number,
-) {
-	const [signX, signY] = getAxesSign(drawnLogosCount)
-
-	const [circleCenterX, circleCenterY] = getCircleCenterCoordinates(
-		signX,
-		signY,
-		width,
-		height,
-	)
-	drawLogoCircleBackground(
-		drawnLogosCount,
-		circleCenterX,
-		circleCenterY,
-		circleStrokeColor,
-		context,
-	)
-
-	const [x, y] = getLogoTopLeftCoordinates(
-		signX,
-		signY,
-		width,
-		height,
-	)
-	context.drawImage(
-		logo,
-		x,
-		y,
-		settings.logo.image.side,
-		settings.logo.image.side,
-	)
-}
-
-/**
- * Returns an array containing the sign of X and Y axes
- * to identify the quadrant in which the logo must be drawn.
- */
-function getAxesSign(drawnLogosCount: number): [number, number] {
-	// The angle relative to the center of the image that determines the position of the logo
-	// starting from the bottom right (-π/4) and stepping by 90° clockwise (-π/2)
-	const angle = -Math.PI / 4 - Math.PI / 2 * drawnLogosCount
-	return [Math.cos, Math.sin].map((fn) => Math.sign(fn(angle))) as [number, number]
-}
-
-function drawLogoCircleBackground(
-	drawnLogosCount: number,
-	centerX: number,
-	centerY: number,
-	strokeColor: string,
-	context: OffscreenCanvasRenderingContext2D | CanvasRenderingContext2D,
-) {
-	const compensationAngle = 1 / 20
-	const startAngle = Math.PI * 3 / 4
-
-	// Draw the portion of ellipse that serves as border
-	context.beginPath()
-	context.ellipse(
-		centerX,
-		centerY,
-		circleRadius + settings.logo.circle.strokeWidth,
-		circleRadius + settings.logo.circle.strokeWidth,
-		drawnLogosCount * Math.PI / 2,
-		startAngle + compensationAngle,
-		startAngle + Math.PI - compensationAngle,
-	)
-	context.closePath()
-	context.fillStyle = strokeColor
-	context.fill()
-
-	// Dirty hack to hide the remaining portion of the frame in the corner
-	context.beginPath()
-	context.moveTo(centerX, centerY)
-	context.arc(
-		centerX,
-		centerY,
-		circleRadius + settings.logo.circle.strokeWidth,
-		drawnLogosCount * Math.PI / 2 + Math.PI / 4 - compensationAngle,
-		drawnLogosCount * Math.PI / 2 + Math.PI / 4 + compensationAngle,
-	)
-	context.closePath()
-	context.fillStyle = settings.logo.circle.color
-	context.fill()
-	
-	// Draw the actual circle
-	context.beginPath()
-	context.arc(
-		centerX,
-		centerY,
-		circleRadius,
-		0,
-		Math.PI * 2,
-	)
-	context.closePath()
-	context.fillStyle = settings.logo.circle.color
-	context.fill()
-}
-
-function getCircleCenterCoordinates(
-	signX: number,
-	signY: number,
-	width: number,
-	height: number,
-): [number, number] {
-	return [
-		width / 2 + signX * (width / 2 - settings.logo.circle.margin - Math.floor(settings.logo.circle.strokeWidth / 2) - circleRadius),
-		height / 2 - signY * (height / 2 - settings.logo.circle.margin - Math.floor(settings.logo.circle.strokeWidth / 2) - circleRadius),
-	]
-}
-
-function getLogoTopLeftCoordinates(
-	signX: number,
-	signY: number,
-	width: number,
-	height: number,
-): [number, number] {
-	return [
-		width / 2 + signX * (width / 2 - logoMargin - (signX > 0 ? settings.logo.image.side : 0)),
-		height / 2 - signY * (height / 2 - logoMargin - (signY < 0 ? settings.logo.image.side : 0)),
-	]
-}
-
-async function getBlobAndDestroy(canvas: OffscreenCanvas | HTMLCanvasElement): Promise<Blob> {
-	if (canvas instanceof HTMLCanvasElement) {
-		return new Promise((resolve, reject) => {
-			canvas.toBlob((blob) => {
-				if (blob === null) {
-					reject()
-					return
-				}
-				canvas.remove()
-				resolve(blob)
-			})
-		})
-	}
-	return canvas.convertToBlob()
 }
 
 /**
